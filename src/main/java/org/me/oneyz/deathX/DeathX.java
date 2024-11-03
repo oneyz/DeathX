e592fda0ed99ff0dc3d50d2cc41f6898ac0a87de
Avzti <avzti@none.com>
2024-11-03 16:42:17 +0100

    Initial Commit

--- src/main/java/org/me/oneyz/deathX/DeathX.java
+++ src/main/java/org/me/oneyz/deathX/DeathX.java
@@ -7,7 +7,7 @@ public final class DeathX extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        System.out.println("Test");
     }
 
     @Override
