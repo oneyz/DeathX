cd8085cfa24fcd09ce9fb18ca16b183a78b62e69
Avzti <avzti@none.com>
2024-11-03 16:41:22 +0100

    Initial Commit

---
+++ src/main/java/org/me/oneyz/deathX/DeathX.java
@@ -0,0 +1,17 @@
+package org.me.oneyz.deathX;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DeathX extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
